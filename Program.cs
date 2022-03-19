114eeb1c9b71beb4abff643312159cdcbc597da3
kappu1999 <74296849+kappu1999@users.noreply.github.com>
2022-03-19 20:09:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace asynchronousassignment
+{
+    public class Program
+    {
+        static async Task <string[]> Read(string Filename)
+        {
+            var ReadData = await File.ReadAllLinesAsync(Filename);
+            return ReadData; 
+        }
+        static async Task Write(string[] WriteData,string Filename)
+        {
+            await File.WriteAllLinesAsync(Filename, WriteData);
+            System.Console.WriteLine("File A.txt data copy successfully in B.txt");
+        }
+        public static async Task Copy()
+        {
+            var Content = Read("A.txt");
+            await Write(Content.Result,"B.txt");
+        }
+        public static void Main(string[] args)
+        {
+            Task task = Copy();       
+        }
+            
+    }
+}
+
